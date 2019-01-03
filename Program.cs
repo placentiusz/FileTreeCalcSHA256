c5954b18977fff10f178394491cce27bc41e535f
placentiusz <jacek@jablko.pl>
2019-01-03 16:15:15 +0100

    Initial

---
+++ Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+
+namespace sumgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TraverseTree(args[0]);
+
+            Console.WriteLine("Press any key");
+            Console.ReadKey();
+        }
+        public static void TraverseTree(string root)
+        {
+            // Data structure to hold names of subfolders to be
+            // examined for files.
+            Stack<string> dirs = new Stack<string>(20);
+
+            if (!System.IO.Directory.Exists(root))
+            {
+                throw new ArgumentException();
+            }
+            dirs.Push(root);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+                string[] subDirs;
+                try
+                {
+                    subDirs = System.IO.Directory.GetDirectories(currentDir);
+                }
+                // An UnauthorizedAccessException exception will be thrown if we do not have
+                // discovery permission on a folder or file. It may or may not be acceptable 
+                // to ignore the exception and continue enumerating the remaining files and 
+                // folders. It is also possible (but unlikely) that a DirectoryNotFound exception 
+                // will be raised. This will happen if currentDir has been deleted by
+                // another application or thread after our call to Directory.Exists. The 
+                // choice of which exceptions to catch depends entirely on the specific task 
+                // you are intending to perform and also on how much you know with certainty 
+                // about the systems on which this code will run.
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                string[] files = null;
+                try
+                {
+                    files = System.IO.Directory.GetFiles(currentDir);
+                }
+
+                catch (UnauthorizedAccessException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                catch (System.IO.DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                // Perform the required action on each file here.
+                // Modify this block to perform your required task.
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        // Perform whatever action is required in your scenario.
+                        System.IO.FileInfo fi = new System.IO.FileInfo(file);
+                        Console.WriteLine("FileName {0}: Size:{1}, Creation:{2}", fi.Name, fi.Length, fi.CreationTime);
+                        Console.WriteLine(CalculateSHA256(file));
+                    }
+                    catch (System.IO.FileNotFoundException e)
+                    {
+                        // If file was deleted by a separate application
+                        //  or thread since the call to TraverseTree()
+                        // then just continue.
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
+                }
+
+                // Push the subdirectories onto the stack for traversal.
+                // This could also be done before handing the files.
+                foreach (string str in subDirs)
+                    dirs.Push(str);
+            }
+        }
+
+        public static string CalculateSHA256(string filename)
+        {
+            using (SHA256 mySHA256 = SHA256.Create())
+            {
+                using (FileStream stream = File.OpenRead(filename))
+                {
+                    try
+                    {
+                        byte[] hashValue = mySHA256.ComputeHash(stream);
+                        return BitConverter.ToString(hashValue).Replace("-","").ToLowerInvariant();
+                    }
+                    catch (IOException e)
+                    {
+                        return ($"I/O Exception: {e.Message}").ToString();
+                    }
+                    catch (UnauthorizedAccessException e)
+                    {
+                        return ($"Access Exception: {e.Message}").ToString();
+                    }
+                }
+            }
+        }
+        public static void PrintByteArray(byte[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]:X2}");
+                if ((i % 4) == 3) Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
